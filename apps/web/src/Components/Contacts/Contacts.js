354450c5757a26203cc53fe58467af6f991f5a17
Eduard <alehined@mail.ru>
2022-09-25 19:57:13 +0300

    add router, add navbar,add contacts

---
+++ apps/web/src/Components/Contacts/Contacts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Contacts.css'
+
+function Contacts() {
+    return (
+        <>
+            <h1 className="contacts">Контакты</h1>
+
+            <p className="contacts-item">+7 (812) 123-45-67</p>
+            <p className="contacts-item">+7 (911) 123-45-67</p>
+            <p className="contacts-item">Новоостровский проспект, дом 36 лит. С</p>
+        </>
+    )
+}
+export default Contacts
