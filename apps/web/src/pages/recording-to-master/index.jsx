4241934aa001b1a361179cca7032c51e55e0ebb7
Alexander <impoossible.777@gmail.com>
2022-09-27 16:53:46 +0400

    pages recordingtomaster: replaced images with placeholder

--- apps/web/src/pages/recording-to-master/index.jsx
+++ apps/web/src/pages/recording-to-master/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import photo from './img/anna.jpg';
 import PriceList from '../../Components/PriceList';
 import './style.css';
 
@@ -36,17 +35,24 @@ function RecordingToMaster() {
       <div className="recording-to-master__info">
         <div className="recording-to-master__master-info">
           <div className="recording-to-master__master-name">
-            Андреева Анна Анитина
+            Фамилия Имя Отчество
           </div>
           <img
             className="recording-to-master__master-photo"
-            src={photo}
+            src="https://placehold.co/400x477?text=Фото+мастера"
             alt="Фото"
           />
         </div>
         <div className="recording-to-master__wrapp-price-list">
           <PriceList services={services} />
         </div>
+      </div>
+      <div className="recording-to-master__calendar">
+        <img
+          className="recording-to-master__img"
+          src="https://placehold.co/700x200?text=Часы+записи+для+клиентов"
+          alt="Часы для записи"
+        />
       </div>
     </div>
   )
