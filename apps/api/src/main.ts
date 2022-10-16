a8aeea09ee9792c6715effb447c6a3fd6bbde0e6
dp <regs@rigtaf.ru>
2022-10-16 19:48:33 +0300

    chore: production deployment

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.NODE_ENV == 'production' ? 80 : 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
