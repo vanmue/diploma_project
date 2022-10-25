3ea02fea5d4cebbb6dc959b19a07bd5db292f2c7
dp <regs@rigtaf.ru>
2022-10-26 02:04:59 +0300

    refactor: containers

--- docker.prod.api.up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker compose --project-name diploma-project-api-servers \
-    --project-directory . \
-    --file .docker/prod/docker-compose.api.yml \
-    up \
-    --remove-orphans \
-    --build \
-    --detach \
