d4efbb34ed2aa469330a5f1dc9595cdee827b16d
Alexander <impoossible.777@gmail.com>
2022-11-24 21:15:07 +0400

    pages salonspage,salonpage,masterspage connect to db not finished

--- apps/web/src/actions/citiesActions.js
+++ apps/web/src/actions/citiesActions.js
@@ -1,2 +1,25 @@
+export const GET_ALL_CITIES = '@@cities/GET_ALL_CITIES';
 export const GET_ALL_CITIES_SUCCESS = '@@cities/GET_ALL_CITIES_SUCCESS';
+
+/**
+ * Массив всех городов
+ * @params {array} date - массив объектов  с городами
+*/
+export const getCitiesAction = (date) => ({
+  type: GET_ALL_CITIES,
+  payload: date
+});
+
+/**
+ * Get запрос на получение  всех городов
+*/
+export const getCitiesThunk = () => async (dispatch, getState) => {
+  fetch('/api/v1/cities')
+    .then(req => req.json())
+    .then(res => {
+      // console.log('getCitiesThunk: ', res.data)
+      dispatch(getCitiesAction(res.data));
+    })
+  // .catch(console.log('getCitiesThunk: ', 'Что-то не получилось'));
+}
 
--- apps/web/src/reducers/mastersReducer.js
+++ apps/web/src/reducers/mastersReducer.js
@@ -1,24 +1,37 @@
+import {
+  GET_ALL_MASTERS,
+  GET_ALL_MASTERS_FOR_ACTIVE_SALON
+} from "../actions/mastersActions";
 // import {
 //   CHANGE_ACTIVE_PAGE_FOR_PAGINATION,
 // } from "../actions/salonsAction";
 
-// const initialStore = {
-//   masters: [
-//     {
-//       rating: 4.5
-//     }
-//   ]
-// }
+const initialStore = {
+  masters: [],
+  mastersActiveSalon: null,
+  activePage: 1
+  // masters: [
+  //   {
+  //     rating: 4.5
+  //   }
+  // ]
+}
 
-// export default function salonsReducer(store = initialStore, action) {
-//   switch (action.type) {
-//     case CHANGE_ACTIVE_PAGE_FOR_PAGINATION: {
-//       return {
-//         ...store,
-//         activePage: action.payload
-//       }
-//     }
-//     default:
-//       return store;
-//   }
-// }
+export default function mastersReducer(store = initialStore, action) {
+  switch (action.type) {
+    case GET_ALL_MASTERS: {
+      return {
+        ...store,
+        masters: action.payload
+      }
+    }
+    case GET_ALL_MASTERS_FOR_ACTIVE_SALON: {
+      return {
+        ...store,
+        mastersActiveSalon: action.payload
+      }
+    }
+    default:
+      return store;
+  }
+}
