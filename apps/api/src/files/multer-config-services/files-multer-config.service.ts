c8d3464205a761531f9b627d74989271b780b11f
dp <regs@rigtaf.ru>
2022-11-29 21:46:08 +0300

    fix: file uploads

--- apps/api/src/files/multer-config-services/files-multer-config.service.ts
+++ apps/api/src/files/multer-config-services/files-multer-config.service.ts
@@ -2,6 +2,6 @@ import { AbstractMulterConfigService } from './abstract-multer-config.service';
 
 export class FilesMulterConfigService extends AbstractMulterConfigService {
   protected getSubdirectory(): string {
-    return '/files';
+    return '/uploads';
   }
 }
